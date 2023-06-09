a1ec5c57ef1858d78420d84df8da82c74b6ce93f
xingwg <xing.weiguo@intellif.com>
2023-06-09 15:50:42 +0800

    :sparkles: 新增支持regnet, swin_v2

---
+++ src/models/regnet.h
@@ -0,0 +1,16 @@
+//
+// Created by intellif on 23-6-9.
+//
+
+#ifndef DCL_WRAPPER_REGNET_H
+#define DCL_WRAPPER_REGNET_H
+
+#include "base/base_classification.h"
+
+namespace dcl {
+    class RegNet : public BaseClassifier {
+
+    };
+}
+
+#endif //DCL_WRAPPER_REGNET_H
---
+++ src/models/swin_v2.h
@@ -0,0 +1,16 @@
+//
+// Created by intellif on 23-6-9.
+//
+
+#ifndef DCL_WRAPPER_SWIN_V2_H
+#define DCL_WRAPPER_SWIN_V2_H
+
+#include "base/base_classification.h"
+
+namespace dcl {
+    class SwinV2 : public BaseClassifier {
+
+    };
+}
+
+#endif //DCL_WRAPPER_SWIN_V2_H
